b2102fa638494f244236765796d4fa223282210a
Kevin112-zou <2359105796@qq.com>
2025-01-15 14:02:34 +0800

    fix: 标签列表视图类不使用@Component注解会报错

--- src/main/java/com/yupi/yupicturebackend/model/vo/PictureTagCategory.java
+++ src/main/java/com/yupi/yupicturebackend/model/vo/PictureTagCategory.java
@@ -1,12 +1,16 @@
 package com.yupi.yupicturebackend.model.vo;
 
 
-
 import lombok.Data;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+/**
+ * 图片标签分类列表视图
+ */
 @Data
+@Component
 public class PictureTagCategory {
     /**
      * 标签列表
